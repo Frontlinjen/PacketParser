a737e21011478750b591625be2c8760eb742be35
MikkelLeth <mkleth10@gmail.com>
2016-10-25 10:01:25 +0200

    Date Class
    Made the Date class

--- src/IR/Date.java
+++ src/IR/Date.java
@@ -5,4 +5,41 @@ public class Date {
 	private int year;
 	private int month;
 	private int day;
+	
+	public Date(int year, int month, int day){
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public Date(String st){
+		String[] ts = st.split("[-]");
+		year = Integer.parseInt(ts[0]);
+		month = Integer.parseInt(ts[1]);
+		day = Integer.parseInt(ts[2]);
+	}
+	
+	public int getYear(){
+		return year;
+	}
+	
+	public void setYear(int year){
+		this.year = year;
+	}
+	
+	public int getMonth(){
+		return month;
+	}
+	
+	public void setMonth(int month){
+		this.month = month;
+	}
+	
+	public int getDay(){
+		return day;
+	}
+	
+	public void setDay(int day){
+		this.day = day;
+	}
 }
