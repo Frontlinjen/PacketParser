607057713b20c770af0252e9dbd67329b2685fb0
bøllemis <Bruger@miniMonster>
2016-11-01 17:26:36 +0100

    printer =/= done

---
+++ src/IR/PrinterOffDoom.java
@@ -0,0 +1,15 @@
+package IR;
+
+import java.awt.Color;
+
+import javax.swing.text.html.CSS.Attribute;
+
+public class PrinterOffDoom {
+
+	public coloredString(Color color, String text){
+		System.out.println(text, color.);
+	}
+	
+	
+	
+}
